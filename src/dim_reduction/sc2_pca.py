d79636fa41819014259fa3f8c4aa26dcf6a6d8fb
Andrzej Białecki <kaszanas@interia.pl>
2022-08-15 23:20:59 +0200

    Changed dir structure in src, added files

---
+++ src/dim_reduction/sc2_pca.py
@@ -0,0 +1,14 @@
+import numpy as np
+import seaborn as sb
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.decomposition import PCA
+from sklearn import preprocessing
+
+from sklearn import datasets
+
+#%%
+
+
+
+# %%
